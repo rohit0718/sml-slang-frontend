e74d1ce8d4d78e7b56e0d9158bfdff957eccc783
Rohit <rajeshbhatrohit@gmail.com>
2023-03-29 22:42:49 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ export REACT_APP_MODULE_BACKEND_URL=https://source-academy.github.io/modules
 export SW_EXCLUDE_REGEXES='["^/source", "^/sicp", "^/modules", "^/ev3-source"]'
 export PUBLIC_URL='https://rohit0718.github.io/sml-slang-frontend'
 
-git checkout deploy
+git branch -D deploy
+git checkout -b deploy
 yarn install
 yarn run build
 ln -s index.html build/playground.html
@@ -17,6 +18,6 @@ rm -rf build/*
 
 git add .
 git commit -m "deploy"
-git push origin deploy
+git push --force origin deploy
 
 git checkout main
