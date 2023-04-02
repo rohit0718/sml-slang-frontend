6ec630e59f803f4b39855c516655126c53c133a3
Sebastian Toh <sebas.tsj.98@gmail.com>
2023-04-02 22:15:45 +0800

    update run function to use exported function from sml-slang

--- src/sml-integration/run.ts
+++ src/sml-integration/run.ts
@@ -1,13 +1,10 @@
-import { evaluateExp } from 'sml-slang/interpreter/interpreter';
-import { Expression } from 'sml-slang/parser/ast';
-import { parseExp } from 'sml-slang/parser/parser';
+import { parseTypeCheckAndEvaluateExp } from 'sml-slang';
 import { SourceError } from 'sml-slang/types';
 import { Context, Result } from 'sml-slang/types';
 
 export function run(code: string, context: Context): Result {
   try {
-    const exp = parseExp(code) as Expression;
-    return evaluateExp(exp, true);
+    return parseTypeCheckAndEvaluateExp(code, true);
   } catch (error) {
     return { status: 'errored', error: error as SourceError };
   }
