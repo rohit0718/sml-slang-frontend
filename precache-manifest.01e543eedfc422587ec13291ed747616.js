358f8ba550a78cbc36b7078cf488404a0768aee3
Sebastian Toh <sebas.tsj.98@gmail.com>
2023-04-14 09:21:12 +0800

    deploying from d9d825bf

---
+++ precache-manifest.01e543eedfc422587ec13291ed747616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9492d1a61def06b4bb553cf672777044",
+    "url": "https://rohit0718.github.io/sml-slang-frontend/index.html"
+  },
+  {
+    "revision": "1f661fa199ec4d5f9df3",
+    "url": "https://rohit0718.github.io/sml-slang-frontend/static/css/main.f144cae0.css"
+  },
+  {
+    "revision": "1f661fa199ec4d5f9df3",
+    "url": "https://rohit0718.github.io/sml-slang-frontend/static/js/main.7f7db32c.js"
+  },
+  {
+    "revision": "6e744e49dc803503f21522e7db40d0e8",
+    "url": "https://rohit0718.github.io/sml-slang-frontend/static/js/main.7f7db32c.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0023f54056ce3f003909086adae6df6",
+    "url": "https://rohit0718.github.io/sml-slang-frontend/static/media/academy_background.d0023f54.jpg"
+  },
+  {
+    "revision": "ba777b48dc283454fd038bf288cab7c7",
+    "url": "https://rohit0718.github.io/sml-slang-frontend/static/media/login_background.ba777b48.jpg"
+  },
+  {
+    "revision": "61832e2d7d15f774f881ef9d608acf83",
+    "url": "https://rohit0718.github.io/sml-slang-frontend/static/media/no_page_background.61832e2d.jpg"
+  }
+]);
---
+++ precache-manifest.0b61f245a544077102eb1b9588371de2.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac4973dd14163ba6f82dfbb7d592de4",
+    "url": "https://rohit0718.github.io/sml-slang-frontend/index.html"
+  },
+  {
+    "revision": "42e0424bd733e4753eab",
+    "url": "https://rohit0718.github.io/sml-slang-frontend/sw-custom.js"
+  }
+]);
